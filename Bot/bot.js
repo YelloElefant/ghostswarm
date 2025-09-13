97b12c8aa5cdf6012ed2421405c62318f9ddd550
YelloElefant <alextrotternz@gmail.com>
2025-09-14 00:32:24 +0100

    fix __dirname
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Bot/bot.js
+++ Bot/bot.js
@@ -681,8 +681,6 @@ const {
     fileURLToPath
 } = require("url");
 
-const __dirname = path.dirname(fileURLToPath(
-    import.meta.url));
 app.use(express.static(path.join(__dirname, "public")));
 
 // API: status
